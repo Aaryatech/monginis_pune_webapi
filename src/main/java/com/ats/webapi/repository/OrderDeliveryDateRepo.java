5b722b9e376e1aa2f8bda1f047e8ac9ded99e36c
maddy <maddy@maddy-Inspiron-3541>
2019-11-30 16:14:16 +0530

    //maddy

--- src/main/java/com/ats/webapi/repository/OrderDeliveryDateRepo.java
+++ src/main/java/com/ats/webapi/repository/OrderDeliveryDateRepo.java
@@ -10,14 +10,21 @@ import com.ats.webapi.model.OrderDeliveryDate;
 
 public interface OrderDeliveryDateRepo extends JpaRepository<OrderDeliveryDate, Integer> {
 	
-	@Query(value = "SELECT o.fr_id,\n" + 
-					"		m.fr_name,\n" + 
-					"       SUM(o.order_mrp) AS ttl_amt\n" + 
-					"FROM t_order o , m_franchisee m\n" + 
-					"WHERE o.delivery_date BETWEEN :fromDate AND :toDate AND\n" + 
-					"	m.fr_id=o.fr_id\n" + 
-					"    GROUP BY m.fr_id \n" + 
-					"    ORDER BY m.fr_name",nativeQuery=true)
+	@Query(value = "SELECT\n" + 
+			"    o.fr_id,\n" + 
+			"    m.fr_name,\n" + 
+			"    SUM(\n" + 
+			"        (o.order_mrp * `order_qty`) *((100 - is_positive) / 100)\n" + 
+			"    ) AS ttl_amt\n" + 
+			"FROM\n" + 
+			"    t_order o,\n" + 
+			"    m_franchisee m\n" + 
+			"WHERE\n" + 
+			"    o.delivery_date BETWEEN :fromDate AND :toDate AND m.fr_id = o.fr_id\n" + 
+			"GROUP BY\n" + 
+			"    m.fr_id\n" + 
+			"ORDER BY\n" + 
+			"    m.fr_name",nativeQuery=true)
 	public List<OrderDeliveryDate> getOrderByDeliveyDate(@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 
 }
